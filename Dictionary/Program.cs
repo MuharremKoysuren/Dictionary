c45a6f9c94c1daea9ac95af7b7f341ad014954ee
Muharrem KÖysüren <USER@DESKTOP-001LUTQ>
2024-06-05 13:30:07 +0300

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic; 
+
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> il= new Dictionary<int, string>();
+            il.Add(34, "İstanbul");
+            il.Add(06, "Ankara");
+            il.Add(40, "Kırşehir");
+            il.Add(29, "Gümüşhane");
+            il.Add(71, "Kırıkkale");
+            Console.Write("Plaka numarasını giriniz. ");
+            int plaka = int.Parse(Console.ReadLine());
+            Console.Write(il[plaka]);
+            Console.Read(); 
+        }
+    }
+}
